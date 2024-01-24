a44e0c9006aecc929f3231b1d64bc246333d3ec7
basapathitarun <basapathitarunkumar9686@gmail.com>
2024-01-24 08:39:20 +0530

    changes

--- prowler/app.py
+++ prowler/app.py
@@ -17,6 +17,7 @@ from prowler.lib.cli.parser import ProwlerArgumentParser
 from prowler.lib.logger import logger, set_logging_config
 from prowler.lib.outputs.compliance import display_compliance_table
 from prowler.lib.outputs.outputs import extract_findings_statistics
+# from prowler.lib.outputs.summary_table import display_summary_table
 from prowler.providers.common.allowlist import set_provider_allowlist
 from prowler.providers.common.audit_info import (
     set_provider_audit_info,
@@ -136,14 +137,14 @@ def prowler():
     stats = extract_findings_statistics(findings)
 
 
-    # # Display summary table
+    # # # Display summary table
     # if not args.only_logs:
-    #     # display_summary_table(
-    #     #     findings,
-    #     #     audit_info,
-    #     #     audit_output_options,
-    #     #     provider,
-    #     # )
+    #     display_summary_table(
+    #         findings,
+    #         audit_info,
+    #         audit_output_options,
+    #         provider,
+    #     )
 
     if compliance_framework and findings:
         for compliance in compliance_framework:
