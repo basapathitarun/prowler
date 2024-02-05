00a771fd81a9a14430570e5d891d2d93ea578b3c
basapathitarun <basapathitarunkumar9686@gmail.com>
2024-02-05 16:41:04 +0530

    changes

--- prowler/__main__.py
+++ prowler/__main__.py
@@ -107,7 +107,7 @@ def prowler():
     print(f"audit_output_options.output_filename -> {audit_output_options.output_filename}\n")
     # Execute checks
 
-    file_loc = "{audit_output_options.output_filename}"
+    file_loc = audit_output_options.output_filename
     file_loc=file_loc+"_"+compliance_framework+".csv"
     print(file_loc)
     findings = []
