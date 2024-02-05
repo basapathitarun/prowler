2959a4f48fe66f1cfb4f12a31994ee27d75296dc
basapathitarun <basapathitarunkumar9686@gmail.com>
2024-02-05 15:21:57 +0530

    changes

---
+++ test.py
@@ -0,0 +1,31 @@
+import boto3
+import subprocess
+
+# Create a session using the default credential chain
+session = boto3.Session()
+
+# Retrieve the current session credentials
+credentials = session.get_credentials()
+
+# Access Key ID
+access_key_id = credentials.access_key
+
+# Secret Access Key
+secret_access_key = credentials.secret_key
+
+
+# Create an AWS session
+session = boto3.Session(
+    aws_access_key_id=access_key_id,
+    aws_secret_access_key=secret_access_key,
+)
+
+# Configure the AWS CLI with the provided credentials and region
+subprocess.run(['aws', 'configure', 'set', 'aws_access_key_id', access_key_id])
+subprocess.run(['aws', 'configure', 'set', 'aws_secret_access_key', secret_access_key])
+
+
+
+
+
+
